3eadccb4e9560a5ced18cab3b59b84a9520be89f
chenli <chen.li@trs.com.cn>
2018-07-04 11:07:15 +0800

    对小程序端的接口请求统一进行token校验

---
+++ src/main/java/com/yanda/util/Const.java
@@ -0,0 +1,14 @@
+package com.yanda.util;
+
+public class Const {
+	
+	public static final String KEY_DATA = "12345678"; // 加密的密钥
+	public static final String appId = "wx8c025f88b3f63c44";
+	public static final String appSecret = "a308a19541497abdab9a2cad360188d3";
+	public static final String CODE_KEY_PRE = "message#code#"; // 验证码存储key前缀
+	public static final String TOKEN_KEY_PRE = "token#"; // 验证码存储key前缀
+	public static final String USER_KEY_PRE = "loginUser#"; // 验证码存储key前缀
+	public static final int CODE_EXPIRE = 2; // 验证码有效期，单位分钟
+	public static final int TOKEN_EXPIRE = 2; // 验证码有效期，单位天
+	
+}
